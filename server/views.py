c8cfe77786bb2b7228c42ba15cf5eb652c7b7864
Michael Pavlukhin <alivemihail@gmail.com>
2018-04-20 20:52:00 +0300

    afsfas

--- server/views.py
+++ server/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from server.forms import SignUpForm, LoginForm
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return "You did it!!!!"
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def signin(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+    
