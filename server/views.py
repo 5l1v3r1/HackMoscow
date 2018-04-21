2aefe68b0f75c5621cd04c46ac29b75acc4288f0
Michael Pavlukhin <alivemihail@gmail.com>
2018-04-21 12:00:42 +0300

    Fix

--- server/views.py
+++ server/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect, HttpResponse
 from server.forms import SignUpForm, LoginForm, CreateTeamForm
-from .models import Profile, Hackathon, Team, Skill
+from .models import Profile, Hackathon, Team, Skill, Tag
 from django.shortcuts import render, redirect, HttpResponse, get_object_or_404, HttpResponseRedirect
 
 from server.forms import SignUpForm, LoginForm, NewHackathonForm, ApplyToHack
