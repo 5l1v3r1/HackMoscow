71d495a5e0dc5aa20671c434d484d959aaa110ff
Alexander Karpin <Karpin_Aleksandr_1999@mail.ru>
2018-04-21 08:32:13 +0300

    hack updated

--- server/views.py
+++ server/views.py
@@ -6,9 +6,13 @@ from django.shortcuts import render, redirect, HttpResponse
 from server.forms import SignUpForm, LoginForm, CreateTeamForm
 from .models import Profile, Hackathon, Team
 from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
-from server.forms import SignUpForm, LoginForm, NewHackathonForm
+from server.forms import SignUpForm, LoginForm, NewHackathonForm, ApplyToHack
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 from django.forms.formsets import formset_factory
+from server.models import Hackathon
+from django.forms.models import model_to_dict
 
 
 def user_info(request):
@@ -17,49 +21,17 @@ def user_info(request):
 		return render(request, 'profile.html', )
 
 
-def all_hackathones(request):
-	hacks = Hackathon.objects.all()
-	return render(request, 'hacks.html', {'hacks': hacks})
-
-
-def signup(request):
-	if request.method == 'POST':
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			user.refresh_from_db()
-			user.save()
-			username = form.cleaned_data.get('username')
-			raw_password = form.cleaned_data.get('password1')
-			user = authenticate(username=username, password=raw_password)
-			login(request, user)
-			return "You did it!!!!"
-	else:
-		form = SignUpForm()
-	return render(request, 'signup.html', {'form': form})
-
-
-def signin(request):
-	if request.method == 'POST':
-		form = LoginForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-
-	else:
-		form = LoginForm()
-	return render(request, 'login.html', {'form': form})
-
-#region Team
+# region Team
 def team_info(request, team_id):
 	'''provides information for team info page'''
-	team = Team.objects.all().filter(id = team_id).first()
+	team = Team.objects.all().filter(id=team_id).first()
 	if team != None:
 		users = [user for user in team.users.all()]
-		return render(request, 'team_info.html', {'team':team, 'users': users})
+		return render(request, 'team_info.html', {'team': team, 'users': users})
 	else:
 		return HttpResponse("403")
 
-
+@login_required
 def create_team(request):
 	'''creates team'''
 	if request.method == 'POST':
@@ -69,7 +41,7 @@ def create_team(request):
 				team = Team.objects.create()
 				team.name = team_form.cleaned_data.get('name')
 				for i in range(3):
-					form_username = team_form.cleaned_data.get('participant' + (str)(i+1))
+					form_username = team_form.cleaned_data.get('participant' + (str)(i + 1))
 					user = User.objects.all().filter(username=form_username).first()
 					if user != None:
 						team.users.add(user)
@@ -82,13 +54,11 @@ def create_team(request):
 
 	else:
 		team_form = CreateTeamForm()
-	return render(request, 'create_team.html', {'form':team_form})
-
-#endregion
+	return render(request, 'create_team.html', {'form': team_form})
 
 
-from server.models import Hackathon
-from django.forms.models import model_to_dict
+# endregion
+
 
 
 # Create your views here.
@@ -129,76 +99,54 @@ def signin(request):
 		form = LoginForm()
 	return render(request, 'login.html', {'form': form})
 
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            try:
-                username = form.cleaned_data.get('username')
-                raw_password = form.cleaned_data.get('password')
-                user = authenticate(username=username, password=raw_password)
-                if user:
-                    login(request, user)
-                    return HttpResponse("200")
-                else:
-                    return HttpResponse("403")
-            except Exception as e:
-                print(e)
-                return HttpResponse("403")
-    else:
-        form = LoginForm()
-    return render(request, 'login.html', {'form': form})
 
 
 
-'''View for new hackathons'''
+
 def new_hackathon(request):
-    if request.method == 'POST':
-        form = NewHackathonForm(request.POST)
-        if form.is_valid():
-            hackathon = form.save()
-            hackathon.refresh_from_db()
-            hackathon.save()
+	if request.method == 'POST':
+		form = NewHackathonForm(request.POST)
+		if form.is_valid():
+			hackathon = form.save()
+			hackathon.refresh_from_db()
+			hackathon.save()
+			return HttpResponse("Hackathon created!")
+	else:
+		form = NewHackathonForm()
+	return render(request, 'new_hackathon.html', {'form': form})
 
-            return HttpResponse("Hackathon created!")
-    else:
-        form = NewHackathonForm()
-    return render(request, 'new_hackathon.html', {'form': form})
 
 def hackaton_list(request):
-    hackatons = Hackathon.objects.all()
-    return render(request, 'hackaton_list.html', {'hacks':hackatons})
+	hackatons = Hackathon.objects.all()
+	return render(request, 'hackaton_list.html', {'hacks': hackatons})
 
 
 def hackathon_page(request, id):
-    try:
-        hack = Hackathon.objects.get(id=id)
-        return render(request, 'hackaton', {'hack':hack} )
-    except:
-        return HttpResponse("404")
+	try:
+		hack = Hackathon.objects.get(id=id)
+		return render(request, 'hackaton', {'hack': hack})
+	except:
+		return HttpResponse("404")
 
 
 '''change hackathon view'''
+
+
 def change_hackathon(request, id):
-    hackaton = get_object_or_404(Hackathon, id=id)
-    if request.method == 'POST':
-        form = NewHackathonForm(request.POST)
-        if form.is_valid():
-            tmp = form.instance
-            hackaton.date = tmp.date
-            hackaton.name = tmp.name
-            hackaton.description = tmp.description
-            hackaton.duration = tmp.duration
-            hackaton.max_members = tmp.max_members
+	hackaton = get_object_or_404(Hackathon, id=id)
+	if request.method == 'POST':
+		form = NewHackathonForm(request.POST)
+		if form.is_valid():
+			tmp = form.instance
+			hackaton.date = tmp.date
+			hackaton.name = tmp.name
+			hackaton.description = tmp.description
+			hackaton.duration = tmp.duration
+			hackaton.max_members = tmp.max_members
 
-            hackaton.save()
+			hackaton.save()
 
-            return HttpResponse("Changes saved!")
-    else:
-        form = NewHackathonForm(instance=hackaton)
-    return render(request, 'change_hack_info.html', {'form': form})
-
-
-
-
-    
-
+			return HttpResponse("Changes saved!")
+	else:
+		form = NewHackathonForm(instance=hackaton)
+	return render(request, 'change_hack_info.html', {'form': form})
