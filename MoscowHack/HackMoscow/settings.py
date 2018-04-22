0b3f6bde952237de684d5c8467f090f2b097ba1e
Michael Pavlukhin <alivemihail@gmail.com>
2018-04-22 04:12:39 +0300

    for deploy

--- MoscowHack/HackMoscow/settings.py
+++ MoscowHack/HackMoscow/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=rnje^l&m&1981o82drkywcicu2vqek!1x%=dlx#&)%!nqwumt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '52.191.14.190']
 
 
 # Application definition
